dd7ddb13579cd848054391bd52cdfac60daa654a
Your Name <you@example.com>
2018-05-30 01:20:40 +0300

    push all about

---
+++ src/services/StillerService.js
@@ -0,0 +1,21 @@
+
+
+export const StillerService = {
+    install: function (Vue, options) {
+        //private
+        var myPrivateProperty = 'Private property';
+        //instance properties and methods
+        Vue.prototype.$myPublicProperty = 'Public Property';
+        Vue.prototype.$myAddedMethod = function () {
+            return myPrivateProperty;
+        };
+        Vue.prototype._getData = function (url) {
+            return url;
+        };
+        //static properties and methods
+        Vue.myStaticProperty = 'My static prop';
+        Vue.myStaticMethod = function () {
+            console.log('in');
+        };
+    }
+};
